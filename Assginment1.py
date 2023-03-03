bf364868a2842548e7c4251e557aad4a17eb80b7
Agnikiran <125629077+Agnikiran@users.noreply.github.com>
2023-03-03 07:38:25 +0530

    Add files via upload

---
+++ Assginment1.py
@@ -0,0 +1,9 @@
+
+p=input("Enter the principal ammount:")
+t=input("Enter the time:")
+r=input("Enter the rate:")
+p=float(p)
+t=float(t)
+r=float(r)
+interest=(p*t*r)/100
+print("simple interest is:",interest)
